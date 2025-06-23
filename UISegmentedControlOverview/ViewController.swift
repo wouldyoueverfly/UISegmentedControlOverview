5f1e119bc6d5ea0060aed25d93389a8c0e18354f
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-23 11:39:55 +0300

    []

--- UISegmentedControlOverview/ViewController.swift
+++ UISegmentedControlOverview/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
     }
 
 
+    @IBAction func segmentAction(_ sender: Any) {
+        
+        label.isHidden = false
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            label.text = "First"
+            label.textColor = .magenta
+        case 1:
+            label.text = "Second"
+            label.textColor = .cyan
+        case 2:
+            label.text = "Third"
+            label.textColor = .orange
+        default:
+            print("No text")
+        }
+    }
 }
 
